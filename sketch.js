9895838d93e1000f2a50128097d5b2c45283b0f7
a-johanson <arnej@pixelblock.de>
2021-11-01 22:14:14 +0100

    expand billboards in vertex shader

--- sketch.js
+++ sketch.js
@@ -1,13 +1,20 @@
 import * as THREE from "./three.module.js";
+import SimplexNoise from "./simplex-noise.js";
 
-function pointOnSphere(y, phi) {
+function pointOnSphere(y, phi, simplex) {
     const r = Math.sqrt(1.0 - y*y);
     const x = r * Math.cos(phi);
     const z = r * Math.sin(phi);
-    return [x, y, z];
+    const noiseC     = 5.48;
+    const noiseScale = 1.8;
+    const noiseMag   = 0.025;
+    const m = noiseMag * (2.0 * simplex.noise3D(noiseScale * (x + noiseC), noiseScale * (y + noiseC), noiseScale * (z + noiseC)) - 1.0) + 1.0;
+    return [m * x, m * y, m * z];
 }
 
 function main() {
+    const simplex = new SimplexNoise("seeeeed");
+
     const width = 600;
     const height = 600;
     const canvas = document.querySelector("#c");
@@ -19,64 +26,62 @@ function main() {
     const near = 0.1;
     const far = 100;
     const camera = new THREE.PerspectiveCamera(fov, width / height, near, far);
-    camera.position.z = 1;
+    camera.position.z = 2;
     camera.position.x = 0;
     camera.lookAt(0, 0, 0);
 
     const scene = new THREE.Scene();
 
-    // const y = THREE.MathUtils.randFloatSpread(2);
-    // const phi = THREE.MathUtils.randFloatSpread(2.0 * Math.PI);
-    // const p = pointOnSphere(y, phi);
+    const pointCount = 30000;
+    const vertices = new Float32Array(pointCount * 4 * 3);
+    const faces = new Uint32Array(pointCount * 2 * 3);
+    for (let i = 0; i < pointCount; i++) {
+        const y = THREE.MathUtils.randFloatSpread(2);
+        const phi = THREE.MathUtils.randFloatSpread(2.0 * Math.PI);
+        const p = pointOnSphere(y, phi, simplex);
+
+        const v_base = i * 4;
+        const v_offset = v_base * 3;
+        vertices[v_offset + 0] = p[0];
+        vertices[v_offset + 1] = p[1];
+        vertices[v_offset + 2] = p[2];
+        vertices[v_offset + 3] = p[0];
+        vertices[v_offset + 4] = p[1];
+        vertices[v_offset + 5] = p[2];
+        vertices[v_offset + 6] = p[0];
+        vertices[v_offset + 7] = p[1];
+        vertices[v_offset + 8] = p[2];
+        vertices[v_offset + 9] = p[0];
+        vertices[v_offset + 10] = p[1];
+        vertices[v_offset + 11] = p[2];
+
+        const f_offset = i * 2 * 3;
+        faces[f_offset + 0] = v_base + 0;
+        faces[f_offset + 1] = v_base + 1;
+        faces[f_offset + 2] = v_base + 2;
+        faces[f_offset + 3] = v_base + 0;
+        faces[f_offset + 4] = v_base + 2;
+        faces[f_offset + 5] = v_base + 3;
+    }
 
     const geometry = new THREE.BufferGeometry();
-    const vertices = new Float32Array( [
-        -1.0, -1.0,  0.0,
-        1.0, -1.0,  0.0,
-        1.0,  1.0,  0.0,
-        -1.0,  1.0,  0.0
-    ] );
-    const normals = new Float32Array( [
-        0, 0, 1,
-        0, 0, 1,
-        0, 0, 1,
-        0, 0, 1
-    ] );
-    const uv = new Float32Array( [
-        -1, -1,
-        1, -1,
-        1, 1,
-        -1, 1
-    ] );
-    const faces = new Uint32Array([
-        0, 1, 2,
-        0, 2, 3
-    ]);
-
     geometry.setIndex(new THREE.BufferAttribute(faces, 1));
     geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
-    geometry.setAttribute("normal", new THREE.BufferAttribute(normals, 3));
-    geometry.setAttribute("uv", new THREE.BufferAttribute(uv, 2));
 
     // const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
 
     const material = new THREE.ShaderMaterial({
+        uniforms: {
+            pointRadius: { value: 0.005 }
+        },
         vertexShader: `
+        uniform float pointRadius;
         varying vec2 v_uv;
         void main() {
-            mat4 mv = mat4(modelViewMatrix);
-            mv[0][0] = 1.0;
-            mv[0][1] = 0.0;
-            mv[0][2] = 0.0;
-            mv[1][0] = 0.0;
-            mv[1][1] = 1.0;
-            mv[1][2] = 0.0;
-            mv[2][0] = 0.0;
-            mv[2][1] = 0.0;
-            mv[2][2] = 1.0;
-            vec4 mvPosition = mv * vec4(position, 1.0);
-            gl_Position = projectionMatrix * mvPosition;
-            v_uv = uv;
+            int v_i = gl_VertexID % 4;
+            v_uv = vec2((v_i == 0 || v_i == 3) ? -1.0 : 1.0, (v_i <= 1) ? -1.0 : 1.0);
+            vec4 mv_p = modelViewMatrix * vec4(position, 1.0) + vec4(pointRadius * v_uv, 0.0, 0.0);
+            gl_Position = projectionMatrix * mv_p;
         }`,
         fragmentShader: `
         varying vec2 v_uv;
@@ -96,7 +101,7 @@ function main() {
     function render(time) {
         time *= 0.001;  // convert time to seconds
 
-        mesh.rotation.y = 0.5 * time;
+        mesh.rotation.y = 0.2 * time;
 
         renderer.render(scene, camera);
         requestAnimationFrame(render);
